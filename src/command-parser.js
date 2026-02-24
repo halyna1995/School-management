0edad66186c494d1f9277f2cdda429bee69d62e6
Halyna Romanyshyn <30292604+halyna1995@users.noreply.github.com>
2026-02-24 18:19:18 +0100

    Complete project

--- src/command-parser.js
+++ src/command-parser.js
@@ -1,16 +1,16 @@
 export function parseCommand(userInput) {
   const line = String(userInput).trim();
 
-  if (line === "") {
+  if (line === '') {
     return { command: null, subcommand: null, args: [] };
   }
 
-   // Split by one-or-more whitespace characters
-    const parts = line.split(/\s+/);
+  // Split by one-or-more whitespace characters
+  const parts = line.split(/\s+/);
 
-    const command = parts[0] ? parts[0].toUpperCase() : null;
-    const subcommand = parts[1] ? parts[1].toUpperCase() :  null;
-    const args = parts.slice(2);
+  const command = parts[0] ? parts[0].toUpperCase() : null;
+  const subcommand = parts[1] ? parts[1].toUpperCase() : null;
+  const args = parts.slice(2);
 
   return { command, subcommand, args };
 }
